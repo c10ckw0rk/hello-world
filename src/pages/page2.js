8e6ad778aaa6ba6a84af63a39278401b000f66ce
Daniel Peterson <daniel.peterson@massive.co>
2018-10-11 17:24:49 +1100

    inital commit

---
+++ src/pages/page2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class page2 extends React.Component {
+  static propTypes = {};
+  static defaultProps = {};
+
+  render() {
+    const {} = this.props;
+    return (<div>Page2</div>);
+  }
+}
